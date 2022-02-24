0ddc622b4671466773531f1a08402e26d49a95fe
KAD105 <kazem_abdi@yahoo.com>
2022-02-24 10:20:32 +0100

    Config.yaml added

--- scripts/deploy.py
+++ scripts/deploy.py
@@ -1,10 +1,18 @@
-from brownie import accounts
+from brownie import accounts, config, SimpleStorage
 
 
 def simple_storage_deplpoy():
-    # account = accounts[0]
-    account = accounts.load("freecodecamp-account")
-    print(account)
+    account = accounts[0]
+    # account = accounts.load("freecodecamp-account")
+    # account = accounts.add(config["wallets"]["from_key"])
+
+    simple_storage = SimpleStorage.deploy({"from": account})
+    stored_value = simple_storage.retrieve()
+    print(stored_value)
+    transaction = simple_storage.store(15, {"from": account})
+    transaction.wait(1)
+    updated_stored_value = simple_storage.retrieve()
+    print(updated_stored_value)
 
 
 def main():
